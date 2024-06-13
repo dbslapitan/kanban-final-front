a81f0e76077ea8634adca79db995d6a5aaab3dbc
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-06-13 01:20:16 -0400

    display data on edit board

--- src/app/@modal/(.)[username]/[boardName]/edit/page.tsx
+++ src/app/@modal/(.)[username]/[boardName]/edit/page.tsx
@@ -1,5 +1,24 @@
-export default async function EditBoardModal(){
-    return (
-        <h1>Edit Board Modal</h1>
-    );
+import { URI } from "@/libs/constants";
+import BoardAction from "@/ui/board-action/board-action";
+import Modal from "@/ui/modal/modal";
+import axios from "axios";
+import { notFound } from "next/navigation";
+
+export default async function EditBoardModal({params}: {params: {boardName: string}}){
+
+    console.log(params.boardName);
+
+    try{
+        const {data: board} = await axios.get(`${URI}/api/v1/preview/board/edit/${params.boardName}`);
+    
+        return (
+            <Modal>
+                <BoardAction data={board}/>
+            </Modal>
+        );
+    }
+    catch(e){
+        console.error(e);
+        notFound();
+    }
 }
