b73ad7fb931b39e0d27aabb43591177d50a4c271
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-06-17 17:53:51 -0400

    add authorization on delete page

--- src/app/@modal/(.)[username]/[boardName]/delete/page.tsx
+++ src/app/@modal/(.)[username]/[boardName]/delete/page.tsx
@@ -2,13 +2,17 @@ import { URI } from "@/libs/constants";
 import { navigate } from "@/libs/server-actions";
 import Delete from "@/ui/delete/delete";
 import Modal from "@/ui/modal/modal";
+import { getSession } from "@auth0/nextjs-auth0";
 import axios from "axios";
+import { headers } from "next/headers";
 import { notFound } from "next/navigation";
 
 export default async function DeleteBoardModal({params}: {params: {username: string, boardName: string}}){
 
+    const session = await getSession();
+
     try{
-        const {data: board} = await axios.get(`${URI}/api/v1/${params.username}/board/${params.boardName}`);
+        const {data: board} = await axios.get(`${URI}/api/v1/${params.username}/board/${params.boardName}`, {headers: {Authorization: `Bearer ${session?.accessToken}`}});
 
         return(
             <Modal>
