d2c2ef1f37a0777665bec495617e8ded00925a65
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2024-06-18 11:52:04 -0400

    fix overflow of titles

--- src/ui/title/title.tsx
+++ src/ui/title/title.tsx
@@ -6,23 +6,25 @@ import style from './title.module.scss';
 import { useContext } from "react";
 import { NavContext } from "../provider/provider";
 
-export default function Title({ boards }: { boards: IBoardNames[] }){
+export default function Title({ boards }: { boards: IBoardNames[] }) {
 
     const { boardName } = useParams();
     const { isNavOpen, setIsNavOpen } = useContext(NavContext);
-    
+
     const selected = boards.find(board => board.slugified === boardName);
 
     const handleClick = () => {
         setIsNavOpen(!isNavOpen);
     };
 
-    return(
-        <h1 className={`${style['title']}`}>
-            <button className={`${style['title__toggle']} ${isNavOpen ? style['title__toggle--up'] : ''}`} onClick={handleClick}>
-                {selected?.name}
-            </button>
-            <span className={`${style['title__text']}`}>{selected?.name}</span>
-        </h1>
+    return (
+        <>
+            <h1 className={`${style['title']}`}>
+                <button className={`${style['title__toggle']} ${isNavOpen ? style['title__toggle--up'] : ''}`} onClick={handleClick}>
+                    {selected?.name}
+                </button>
+            </h1>
+            <h1 className={`${style['title__text']}`}>{selected?.name}</h1>
+        </>
     );
 }
